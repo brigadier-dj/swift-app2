8c802bc0b0aa0c514ec2be546b93991cd3511ac5
Alexander Eurich <alekeurich@gmail.com>
2017-06-15 01:03:35 -0500

    Accept rebuild of App1 into App2. ACE

--- SwiftApp2/ViewController.swift
+++ SwiftApp2/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        coolLabel.text = "Answer : \(Double(text1.text!)! + Double(text2.text!)!)"
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
