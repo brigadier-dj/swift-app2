ef6db70b112483f54befba720c94eb6cbe2f25bc
Alexander Eurich <AlekEurich@alexanders-mbp>
2017-06-15 00:32:30 -0500

    Initial Commit

---
+++ SwiftApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp2
+//
+//  Created by Alexander Eurich on 6/15/17.
+//  Copyright © 2017 Alexander Eurich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
